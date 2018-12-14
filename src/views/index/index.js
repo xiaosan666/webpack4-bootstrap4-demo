5eb14c51fa2d8c2790ad5abd1d025257e15ef52a
yanxiaojun <yanxiaojun617@163.com>
2018-12-14 16:03:45 +0800

    refactor:优化

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -22,14 +22,31 @@ $('form').on('submit', function () {
     return false;
 });
 
+// 判断浏览器是否支持webp格式
+function isSupportWebp(callback) {
+    let img = new Image();
+
+    function getResult(event) {
+        // 如果进入加载且图片宽度为1(通过图片宽度值判断图片是否可以显示)
+        let isSupportWebp = event && event.type === 'load' ? img.width === 1 : false;
+        callback && callback(isSupportWebp);
+    }
+
+    img.onerror = getResult;
+    img.onload = getResult;
+    img.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA='; // 一像素图片
+}
+
+
 /* 动态加载首页背景图，让移动端不加载，让chrome加载webp格式 */
 const u = window.navigator.userAgent;
 const isMobile = u.indexOf('Mobile') !== -1;
-const isChrome = u.indexOf('Chrome') !== -1;
 if (!isMobile) {
-    let src = require('../../assets/img/login.webp');
-    if (!isChrome) {
-        src = require('../../assets/img/login.png');
-    }
-    $('#loginImg').html('<img src="' + src + '" alt="">');
+    isSupportWebp(isSupportWebp => {
+        let src = require('../../assets/img/login.webp');
+        if (!isSupportWebp) {
+            src = require('../../assets/img/login.png');
+        }
+        $('#loginImg').html('<img src="' + src + '" alt="">');
+    });
 }
